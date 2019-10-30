7af8469513a6f7d490e7548df31396a1d6e864fc
TommyUnderbjergAndersen <ta@experiment.dk>
2019-10-30 20:16:43 +0100

    Webserver added

--- main/Shooter/Shooter.ino
+++ main/Shooter/Shooter.ino
@@ -1,6 +1,21 @@
 #include <Ticker.h>
 #include "pitches.h"
 
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+#ifndef STASSID
+#define STASSID "malleparken1_IoT"
+#define STAPSK  "tommyandersen"
+#endif
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
+ESP8266WebServer server(80);
+
 const int buttonPin = 5;     // the number of the pushbutton pin
 bool buttonState = true;         // variable for reading the pushbutton status
 bool prevButtonState = true;
@@ -20,6 +35,32 @@ bool endGame = false;
 
 Ticker t;
 
+// Webserver functions
+
+void handleRoot() {
+  //digitalWrite(led, 1);
+  server.send(200, "text/plain", "hello from esp8266!");
+  //digitalWrite(led, 0);
+}
+
+void handleNotFound() {
+  //digitalWrite(led, 1);
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  //digitalWrite(led, 0);
+}
+
+
 void setup() {
   // initialize the LED pin as an output:
   pinMode(ledPinR, OUTPUT);
@@ -34,10 +75,46 @@ void setup() {
 
   Serial.begin(115200);
   Serial.println("Start...");
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+   // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp8266")) {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/", handleRoot);
+
+  server.on("/inline", []() {
+    server.send(200, "text/plain", "this works as well");
+  });
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+ 
   targetOff();
 }
 
 void loop() {
+  handleHit();
+  server.handleClient();
+  MDNS.update();
+}
+
+void handleHit() {
   //TODO: replace by interrups
   buttonState = digitalRead(buttonPin);
   if (!buttonState && prevButtonState) {
@@ -55,7 +132,7 @@ void loop() {
       ledBlue();
       soundHit();
     }    
-  }
+  }  
 }
 
 void hitInterrupt() {
